f14416fb156871a4cb163738b96a99b6561e7d80
tlallinger <lallingerthomas@gmail.com>
2024-10-15 15:35:10 +0700

    use astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroIcon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [
     tailwind(),
     astroIcon({
